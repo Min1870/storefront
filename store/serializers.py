fa3bc0d05736421648918f397d89f1ac50f2e13c
minkhantzaw <minkhantzaw@g.com>
2024-08-23 12:49:57 +0700

    feature: implement products_list api

---
+++ store/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import Product, Collection
+from decimal import Decimal
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description', 'slug', 'inventory',
+                  'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='calculate_tax')
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='collection-detail'
+    # )
+
+    def calculate_tax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
+
+    
