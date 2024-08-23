8297d9d277ff75512ee6447c26a1511ed9a9d42e
minkhantzaw <minkhantzaw@g.com>
2024-08-23 14:28:54 +0700

    fix: change to class based view

--- store/views.py
+++ store/views.py
@@ -3,71 +3,84 @@ from django.db.models import Count
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 from .models import Product, Collection
 from .serializers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def product_list(request):
-    if request.method == 'GET':
+class ProductList(APIView):
+
+    def get(self, request):
         queryset = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(
             queryset, many=True, context={'request': request})
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_details(request, id):
-    product = get_object_or_404(Product, pk=id)
-    if request.method == 'GET':
+class ProductDetails(APIView):
+
+    def get(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product)
         return Response(serializer.data)
-    elif request.method == 'PUT':
+
+    def put(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         serializer = ProductSerializer(product, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+
+    def delete(self, request, id):
+        product = get_object_or_404(Product, pk=id)
         if product.orderitems.count() > 0:
             return Response({'error': 'Product cannot be deleted bcz it is associated with an order item.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view(['GET', 'POST'])
-def collection_list(request):
-    if request.method == 'GET':
+class CollectionList(APIView):
+
+    def get(self, request):
         queryset = Collection.objects.annotate(
             products_count=Count('products')).all()
         serializer = CollectionSerializer(queryset, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+
+    def post(self, request):
         serializer = CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def collection_detail(request, pk):
-    collection = get_object_or_404(Collection.objects.annotate(
-        products_count=Count('products')), pk=pk)
-    if request.method == 'GET':
+class CollectionDetails(APIView):
+
+    def get(self, request, pk):
+        collection = get_object_or_404(Collection.objects.annotate(
+            products_count=Count('products')), pk=pk)
         serializer = CollectionSerializer(collection)
         return Response(serializer.data)
-    if request.method == 'PUT':
+
+    def put(self, request, pk):
+        collection = get_object_or_404(Collection.objects.annotate(
+            products_count=Count('products')), pk=pk)
         serializer = CollectionSerializer(collection, data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    if request.method == 'DELETE':
+
+    def delete(self, request, pk):
+        collection = get_object_or_404(Collection.objects.annotate(
+            products_count=Count('products')), pk=pk)
         if collection.products.count() > 0:
             return Response({'error': 'Collection cannot be deleted bcz it includes in one or more products'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
         collection.delete()
